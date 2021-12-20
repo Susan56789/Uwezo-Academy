abc80059ce6fc6439c58d9e52a9746fc314880f8
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-20 22:04:14 +0300

    updates

--- server/courses.js
+++ server/courses.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-    url = 'https://www.udemy.com//api-2.0/courses/';
+    url = 'https://www.udemy.com//api-2.0/courses';
 
 
          module.exports=  async  function courses() {
@@ -18,6 +18,7 @@ const fetch = require('node-fetch');
                 "credentials":"same-origin",
                 "cache":"no-cache"
                 }
+               
                 
                 
              })
