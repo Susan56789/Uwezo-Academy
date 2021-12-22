5dedf7accb4588d0a94dc3c2e846d9568001d342
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-22 17:59:40 +0300

    navbar styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
-  {path:'about', component: AboutComponent},
+  {path:'reviews', component: AboutComponent},
   {path:'courses', component: CoursesComponent}
 ];
 
