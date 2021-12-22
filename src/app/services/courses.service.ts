4c38ac2208e4d211bdb985c106be01b9aaf6b3a7
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-22 16:18:17 +0300

    uwezo academy

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { catchError, tap } from 'rxjs';
 import { GlobalData } from '../model/global-data';
 import {map} from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,61 +13,42 @@ export class CoursesService {
 
 
   async getCourses(){
-    const result = await this.http.get<any>('https://aqueous-fjord-24538.herokuapp.com/courses').pipe(
+    return  await this.http.get<any>('https://aqueous-fjord-24538.herokuapp.com/courses').pipe(
       map((res) => {
        // console.log(res);
         let rows = res.results;
-        let re = JSON.stringify(rows)
+       // let re = JSON.stringify(rows)
         //let data = re.split('\n')
-        let data =re.split(/},(?=\S)/);
-        data.splice(0,1);
+        //let data =re.split(/},(?=\S)/);
+       // data.splice(0,1);
 
         let courses:GlobalData[]=[];
          
-        let raw ={} as any;
+       // let raw ={} as any;
 
       //  console.log(data);
         
 
-        data.forEach(row =>{
-          let cols = row.split(/(?=S)/);
+       // data.forEach(row =>{
+        //  let cols = row.split(/(?=S)/);
           
 
           //console.log(cols);
 
-          let order ={
-            title:cols[2],
-            url:cols[3],
-            price:cols[5],
-            headline:cols[22],
-          }
+          //courses.push({
+           // title:cols[2],
+           // url:cols[3],
+           // price:cols[5],
+           // headline:cols[22],
+         // })
+       // })
+         
+        console.log(rows);
 
-          let temp: GlobalData = raw[order.title];
-
-          if(temp){
-            temp.price = order.price + temp.price;
-            temp.headline = order.headline + temp.headline;
-            temp.title = order.title + temp.title;
-            temp.url = order.url + temp.url;
-
-            raw[order.title] = temp;
-          }
-            else{
-              raw[order.title] = order;
-            }
-
-        })
-      //  console.log(raw);
+        return Object.values(rows)
         
-      return Object.values(raw);
+      })    
         
-      })
-    )
-    return result;
-    
-  }
-
-
-
+    )}
 
 }
